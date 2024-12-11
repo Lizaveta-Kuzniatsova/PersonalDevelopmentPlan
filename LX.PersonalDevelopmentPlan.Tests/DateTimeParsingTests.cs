382d5c78768c9e391d6ebbffde74a0090fe8b5d6
Lizaveta-Kuzniatsova <lizaveta.kuzniatsova@leverx.com>
2024-12-11 12:17:08 +0300

    Fix tests naming, remove unnecessary string from Assert.True(), create CreateDateTime() method.

--- LX.PersonalDevelopmentPlan.Tests/DateTimeParsingTests.cs
+++ LX.PersonalDevelopmentPlan.Tests/DateTimeParsingTests.cs
@@ -1,11 +1,12 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Reflection.Metadata.Ecma335;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LX.PersonalDevelopmentPlan.Tests;
 
 public class DateTimeParsingTests
 {
     [Fact]
-    public void ParseFirstStringToDateTime()
+    public void Parse_FirstStringToDateTime_True()
     {
         //Arrange
         string dateString = "7/10/2024 13:09";
@@ -15,29 +16,31 @@ public class DateTimeParsingTests
         bool parseResult = DateTime.TryParse(dateString, out resultDate);
 
         //Assert
-        Assert.True(parseResult, "Date string could not be parsed into a DateTime.");
+        Assert.True(parseResult);
         Assert.Equal(new DateTime(2024, 7, 10, 13, 9, 0), resultDate);
     }
 
     [Fact]
-    public void ParseSecondStringToDateTime()
+    public void Parse_SecondStringToDateTime_True()
     {
         //Arrange
-        string dateString = "8/31/2024 23:00:00";
+        string dateString = "8/31/2024 23:00";
         DateTime resultDate;
 
         //Act
         bool parseResult = DateTime.TryParse(dateString, out resultDate);
 
         //Assert
-        Assert.True(parseResult, "Date string could not be parsed into a DateTime.");
+        Assert.True(parseResult);
         Assert.Equal(new DateTime(2024, 8, 31, 23, 0, 0), resultDate);
     }
+
+    public static DateTime CreateDateTime(params int[] date) => new DateTime(date[0], date[1], date[2], date[3], date[4], date[5]);
 
     [Theory]
     [InlineData("7/10/2024 13:09", 2024, 7, 10, 13, 9, 0)]
     [InlineData("8/31/2024 23:00", 2024, 8, 31, 23, 0, 0)]
-    public void ParseTwoStrings(string date, int year, int month, int day, int hour, int minute, int second)
+    public void Parse_TwoStringsToDateTime_True(string date, params int[] expectedDate)
     {
         //Arrange
         string dateString = date;
@@ -47,7 +50,7 @@ public class DateTimeParsingTests
         bool parseResult = DateTime.TryParse(dateString, out resultDate);
 
         //Assert
-        Assert.True(parseResult, $"Date string {dateString} could not be parsed into a DateTime.");
-        Assert.Equal(new DateTime(year, month, day, hour, minute, second), resultDate);
+        Assert.True(parseResult);
+        Assert.Equal(CreateDateTime(expectedDate), resultDate);
     }
 }
